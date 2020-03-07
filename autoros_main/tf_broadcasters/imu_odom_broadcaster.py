ee4faac797e2a18f987bf42542b23840daf32608
Dikioth <elvis@dikioth.me>
2020-03-07 13:36:34 +0000

    some general fixes

--- autoros_main/tf_broadcasters/imu_odom_broadcaster.py
+++ autoros_main/tf_broadcasters/imu_odom_broadcaster.py
@@ -4,39 +4,51 @@
 This packages subscribe to Tag & Anchorarrays and broadcasts the transformations.
 '''
 
+
+
 import rospy
 import tf
 from localizer_dwm1001.msg import Tag
 from localizer_dwm1001.msg import AnchorArray
+from sensor_msgs.msg import Imu
 
 
-def tag_callback(data):
-    br = tf.TransformBroadcaster()
-    br.sendTransform((data.x, data.y, data.z),
-                     tf.transformations.quaternion_from_euler(0, 0, 0),
-                     rospy.Time.now(),
-                     data.header.frame_id,
-                     "world")
-    
-def anchors_callback(data):
-    br = tf.TransformBroadcaster()
+class Broadcaster:
+    '''
+    This class broadcast the transform of the tags position and orientation.
+    Author: Elvis Rodas
+    '''
+    def __init__(self):
+        self.orient = (0,0,0,0)
+        self.br = tf.TransformBroadcaster()
 
-    for anchor in data.anchors:
-        br.sendTransform((anchor.x, anchor.y, anchor.z),
-                        tf.transformations.quaternion_from_euler(0, 0, 0),
+    def start(self):
+        rospy.init_node('tag_anchors_broadcaster', anonymous=True)
+        rospy.Subscriber("/dwm1001/tag", Tag, self.tag_callback)
+        rospy.Subscriber("/imu/data", Imu, self.imu_callback)
+        rospy.loginfo("Running ...")
+        rospy.spin()
+
+    def imu_callback(self, data):
+        self.orient = (data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
+
+    def tag_callback(self, data):
+        self.br.sendTransform((data.x, data.y, data.z),
+                        self.orient,
                         rospy.Time.now(),
-                        anchor.header.frame_id,
+                        data.header.frame_id,
                         "world")
+        
+    def anchors_callback(self, data):
+        br = tf.TransformBroadcaster()
 
-def listener():
-
-    # Subscribe to Anchors and tags topics
-    rospy.init_node('tag_anchors_broadcaster', anonymous=True)
-    rospy.Subscriber("/dwm1001/tag", Tag, tag_callback)
-    rospy.Subscriber("/dwm1001/anchors", AnchorArray, anchors_callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        for anchor in data.anchors:
+            br.sendTransform((anchor.x, anchor.y, anchor.z),
+                            tf.transformations.quaternion_from_euler(0, 0, 0),
+                            rospy.Time.now(),
+                            anchor.header.frame_id,
+                            "world")
 
 if __name__ == '__main__':
-    listener()
+    bc = Broadcaster();
+    bc.start()
